9860e44e139033897fb940a087ed123aee972843
Imanol Salgado <imasalgadodev@gmail.com>
2021-01-22 23:12:09 -0300

    Cambios para acceso de la base de datos

--- src/main/java/com/imanol/meliExam/repositories/DnaRepository.java
+++ src/main/java/com/imanol/meliExam/repositories/DnaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Map;
 
 public interface DnaRepository extends CrudRepository<Dna, String> {
-    @Query("SELECT mutant, COUNT(*) FROM Dna BY mutant")
+    @Query("SELECT mutant, COUNT(*) FROM Dna GROUP BY mutant")
     Map<Boolean, Integer> getMutantCounts();
 }
